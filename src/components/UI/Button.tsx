56a83521ea31cb371f967799a832477f15373864
Bruno Souza <brunoezully@gmail.com>
2024-03-20 13:53:09 -0300

    refactor: `Button` component to use utility class for styling

---
+++ src/components/UI/Button.tsx
@@ -0,0 +1,26 @@
+import cn from "@/utils/cn";
+
+interface ButtonProps {
+    onClick?: () => void;
+    children: React.ReactNode;
+    color: "green" | "red" | "blue";
+}
+
+function Button({ onClick, children, color }: ButtonProps) {
+    return (
+        <button
+            className={cn(
+                "px-4 py-2 rounded text-black hover:text-white font-semibold",
+                "cursor-pointer shadow-lg transition-all duration-300 ease-in-out",
+                color === "green" ? "border-2 border-green-500 hover:bg-green-500" : "",
+                color === "red" ? "border-2 border-red-500 hover:bg-red-500" : "",
+                color === "blue" ? "border-2 border-blue-500 hover:bg-blue-500" : ""
+            )}
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    );
+}
+
+export default Button;
