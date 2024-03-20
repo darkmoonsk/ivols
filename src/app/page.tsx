3c587939da23a7ef3495477716613fcf425d5f30
Bruno Souza <brunoezully@gmail.com>
2024-03-20 17:57:03 -0300

    add header and footer components to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+import Footer from "@/components/Footer";
+import Logo from "@/components/UI/Logo";
 import VocabularyLearning from "@/components/VocabularyLearning/VocabularyLearning";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <VocabularyLearning />
-    </main>
+    <>
+      <header className="p-12">
+        <Logo />
+      </header>
+      <main className="flex min-h-screen flex-col items-center justify-between p-12">
+        <VocabularyLearning />
+      </main>
+      <Footer />
+    </>
   );
 }
