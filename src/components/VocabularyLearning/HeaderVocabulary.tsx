d7d8ebd62dabd192622e0dac2a0ec0637583058b
Bruno Souza <brunoezully@gmail.com>
2024-03-28 13:26:40 -0300

    fix: typo in header text

--- src/components/VocabularyLearning/HeaderVocabulary.tsx
+++ src/components/VocabularyLearning/HeaderVocabulary.tsx
@@ -3,7 +3,7 @@ function HeaderVocabulary({ wordsCount } : { wordsCount: number }) {
     <div className="flex flex-col md:flex-row items-center md:justify-between py-5 
       px-4 bg-fuchsia-100/50 rounded shadow-sm
     ">
-      <h1 className="text-xl font-bold text-cyan-600">I.VO.L.S - Inteligent Vocabulary Learning System</h1>
+      <h1 className="text-xl font-bold text-cyan-600">I.VO.L.S - Intelligent Vocabulary Learning System</h1>
       <div className="flex gap-2">
         <p className="text-green-500">To learn: 15</p>
         <span className="text-gray-400/50">|</span>
